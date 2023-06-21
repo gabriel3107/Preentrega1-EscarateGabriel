1b73431a97a03749126667a61bc801589927da75
Gabriel Escarate Prieto <gabrielescarate33@gmail.com>
2023-06-21 13:12:11 -0500

    Subiendo proyecto 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Cart from './components/cart'
+import Header from './components/header'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header logo="Espri"/>
+      <Cart/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './style.css';
+import Cart from "../cart";
+
+const Header = ({ logo }) => {
+    return (
+        <header className="header">
+            <a href="/" className="logo">{logo}</a>
+            <input type="checkbox" className="side-menu" id="side-menu" />
+            <label className="hamb" htmlFor ="side-menu">
+                <span className="hamb-line"></span>
+            </label>
+            <nav className="nav">
+                <ul className="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Productos</a></li>
+                    <li><a href="#">Categorias</a></li>
+                    <li><a href="#">Contactos</a></li>
+                    <li><a href="#"><Cart/></a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
